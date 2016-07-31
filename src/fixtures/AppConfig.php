20e02af21355938b89aaf6666f26241b8749406f
pyd <pyd001@gmail.com>
2016-07-31 16:01:59 +0200

    Refactor AppConfig and create unit test.

--- src/fixtures/AppConfig.php
+++ src/fixtures/AppConfig.php
@@ -1,151 +1,193 @@
 <?php
-namespace pyd\testkit;
+namespace pyd\testkit\fixtures;
 
+use pyd\testkit\EventsDispatcher;
 use yii\base\InvalidConfigException;
 
 /**
- * Provide configuration for Yii app creation, including bootstrap files to load
- * and server variable to initialize.
+ * Provide Yii app config, bootstrap files to load and server variable to initialize.
+ *
+ * The @see $globalConfig property must be initialized at creation with an
+ * array or the path to a file returning an array.
  *
  * ```php
- * $configProvider = new AppConfigProviderByPath(['allTestsConfig' => $config]);
- * $testCaseDirPath = '/path/to/testcase/parent/directory';
- * $bootstrapFiles = $configProvider->getBootstrapFiles($testCaseDirPath);
- * $serverVars = $configProvider->getServerVars($testCaseDirPath);
- * $appConfig = $configProvider->getAppConfig($testCaseDirPath);
- * ```
+ * // Each key is a path to a directory of the testing tree and it's value is
+ * // an array of configuration data.
+ * // A Configuration data array can contain 3 kinds of keys:
+ * // - the @see BOOTSTRAP_FILES_KEY key which value is an array of bootstrap files
+ * //       ['/path/to/bootstrap/file_1.php', '/path/to/bootstrap/file_2.php', ...];
+ * // - the @see SERVER_VARS_KEY key which value is an array of server variables to initialize
+ * //       ['SERVER_NAME' => 'http://domain.com', 'SCRIPT_NAME' => 'http://domain.com/index-test.php', ...];
+ * // - @see APP_KEY key which value is an array of config files and or config arrays
+ * //       ['/path/to/yii/app/config/file_1.php', $configArray ], ...];
  *
- * The goal is to work on a global configuration array and extract the relevant
- * parts based on the path to the currently executed test case. This can be used
- * with a basic template app or an advanced one as well.
- * The configuration array should look like:
+ * $globalConfig = [
  *
- * ```php
- * $basePath = '/var/www/html/domain.com';
- * $bootstrapFilesKey = AppConfigProviderByPath::BOOTSTRAP_FILES_KEY;
- * $serverVarsKey = AppConfigProviderByPath::SERVER_VARS_KEY;
- * $appKey = AppConfigProviderByPath::APP_KEY;
- * $config = [
+ *      // config for all test cases of the /var/www/myApp/tests directory
+ *      '/var/www/myApp/tests' => [
  *
- *      $basePath . '/tests' => [
- *          $bootstrapFilesKey => [$basePath . '/common/config/bootstrap.php'],
- *          $appKey => [
- *              $basePath . '/common/config/main.php',
- *              $basePath . '/common/config/main-local.php'
+ *          AppConfig::BOOTSTRAP_FILES_KEY => [
+ *              '/var/www/myApp/common/config/bootstrap.php'
+ *          ],
+ *          AppConfig::APP_KEY => [
+ *              '/var/www/myApp/common/config/main.php',
+ *              '/var/www/myApp/common/config/main-local.php'
  *          ]
  *      ],
  *
- *      $basePath . '/tests/frontend' => [
- *          $bootstrapFilesKey => [$basePath . '/frontend/config/bootstrap.php'],
- *          $serverVarsKey => [
+ *      // config for all test cases of the /var/www/myApp/tests/frontend directory
+ *      '/var/www/myApp/tests/frontend' => [
+ *
+ *          AppConfig::BOOTSTRAP_FILES_KEY => [
+ *              '/var/www/myApp/frontend/config/bootstrap.php'
+ *          ],
+ *          AppConfig::SERVER_VARS_KEY => [
  *              'SERVER_NAME' => 'http://domain.com',
  *              'SCRIPT_NAME' => 'http://domain.com/index-test.php',
- *              'SCRIPT_FILENAME' => $basePath . '/frontend/web/index-test.php',
+ *              'SCRIPT_FILENAME' => '/var/www/myApp/frontend/web/index-test.php',
  *          ],
- *          $appKey => [
- *              $basePath . '/frontend/config/main.php',
- *              $basePath . '/frontend/config/main-local.php'
+ *          AppConfig::APP_KEY => [
+ *              '/var/www/myApp/frontend/config/main.php',
+ *              '/var/www/myApp/frontend/config/main-local.php'
  *          ]
  *      ],
  *
- *      $basePath . '/tests/backend' => [
- *          $bootstrapFilesKey => [$basePath . '/backend/config/bootstrap.php'],
- *          $serverVarsKey => [
+ *      // config for all test cases of the /var/www/myApp/tests/backend directory
+ *      '/var/www/myApp/tests/backend' => [
+ *
+ *          AppConfig::BOOTSTRAP_FILES_KEY => [
+ *              '/var/www/myApp/backend/config/bootstrap.php'
+ *          ],
+ *          AppConfig::SERVER_VARS_KEY => [
  *              'SERVER_NAME' => 'http://backend.domain.com',
  *              'SCRIPT_NAME' => 'http://backend.domain.com/index-test.php',
- *              'SCRIPT_FILENAME' => $basePath . '/backend/web/index-test.php',
+ *              'SCRIPT_FILENAME' => '/var/www/myApp/backend/web/index-test.php',
  *          ],
- *          $appKey => [
- *              $basePath . '/backend/config/main.php',
- *              $basePath . '/backend/config/main-local.php'
+ *          AppConfig::APP_KEY => [
+ *              '/var/www/myApp/backend/config/main.php',
+ *              '/var/www/myApp/backend/config/main-local.php'
  *          ]
- *      ],
- *      $basePath . '/tests/common' => [
- *          ...
  *      ]
- * ]
+ * ];
  * ```
+ *
+ * The @see $testCaseDirPath property is initialized by the @see onSetUpBeforeClass
+ * method.
+ * Note that this class must be registered as an observer of the 'setUpBeforeClass'
+ * before the @see \pyd\testkit\fixtures\App class.
  *
  * @author pyd <pierre.yves.delettre@gmail.com>
  */
-class AppConfigProviderByPath extends \yii\base\Object
+class AppConfig extends \yii\base\Object
 {
     /**
-     * Config array key for bootstrap files
+     * Key for bootstrap files array in the @see $globalConfig
      */
     const BOOTSTRAP_FILES_KEY = 'bootstrap-files';
     /**
-     * Config array key for server variables
+     * Key for server variables array in the @see $globalConfig
      */
     const SERVER_VARS_KEY = 'server-vars';
     /**
-     * Config array key for app config
+     * Key for Yii app config array in the @see $globalConfig
      */
     const APP_KEY = 'app';
+    /**
+     * @var array global config i.e. for all test cases
+     */
+    protected $globalConfig;
+    /**
+     * @var string path to the parent directory of the currently executed test
+     * case
+     */
+    protected $testCaseDirPath;
 
     /**
-     * @var array global - for all tests - configuration
+     * Initialization.
+     *
+     * Verify that the @see $globalConfig was intialized.
+     *
+     * @throws InvalidConfigException
      */
-    protected $allTestsConfig;
-
     public function init()
     {
-        if (null === $this->allTestsConfig) {
-            throw new InvalidConfigException(get_class($this) . '::$allTestsConfig must be initialized.', 10);
+        if (null === $this->globalConfig) {
+            throw new InvalidConfigException(get_class($this) . '::$globalConfig must be initialized.', 10);
         }
     }
 
     /**
-     * Return bootstrap files to load according to the currently executed
-     * test case parent directory.
+     * Handle setUpBeforeClass event.
      *
-     * @param string $testCaseDirPath path to the parent directory of the
-     * currently executed test case
-     * @return array ['/path/to/bootstrap/file/one.php', '/path/to/bootstrap/file/two.php',...]
+     * Initialize @see $testCaseDirPath.
+     *
+     * @param string $testCaseClassName class name of the currently executed
+     * test case
      */
-    public function getBootstrapFiles($testCaseDirPath)
+    public function onSetUpBeforeClass($testCaseClassName)
     {
-        $configByPath = $this->getConfigByPath($testCaseDirPath);
-        if (isset($configByPath[self::BOOTSTRAP_FILES_KEY])) {
-            return $configByPath[self::BOOTSTRAP_FILES_KEY];
-        }
-        return [];
+        $rc = new \ReflectionClass($testCaseClassName);
+        $this->testCaseDirPath = dirname($rc->getFileName());
+        $this->_configByPath = null;
+    }
+
+
+    /**
+     * Return bootstrap files to load according to the $path.
+     *
+     * @return array [
+     *      '/path/to/bootstrap/file/one.php',
+     *      '/path/to/bootstrap/file/two.php',
+     *      ...
+     * ]
+     */
+    public function getBootstrapFiles()
+    {
+        $configByPath = $this->getConfigByPath();
+        return isset($configByPath[self::BOOTSTRAP_FILES_KEY]) ? $configByPath[self::BOOTSTRAP_FILES_KEY] : [];
     }
 
     /**
-     * Return server variables to be initialized according to the currently executed
-     * test case.
+     * Return server variables to be initialized according to the $path.
      *
-     * @return array ['serverVarName' => $serverVarNameValue, 'otherServerVarName' => $otherServerVarNameValue, ...]
+     * @return array [
+     *      'serverVarName' => $serverVarNameValue,
+     *      'otherServerVarName' => $otherServerVarNameValue,
+     *      ...
+     * ]
      */
-    public function getServerVars($path)
+    public function getServerVars()
     {
-        $config = $this->getConfigByPath($path);
-        if (isset($config[self::SERVER_VARS_KEY])) {
-            return  $config[self::SERVER_VARS_KEY];
-        }
-        return [];
+        $config = $this->getConfigByPath();
+        return isset($config[self::SERVER_VARS_KEY]) ? $config[self::SERVER_VARS_KEY] : [];
     }
 
-    /**
-     * Return the configuration array to be used to create the Yii app according
-     * to the currently executed test case.
-     *
-     * @return array
-     */
-    public function getAppConfig($path)
+   /**
+    * Return the configuration used to create the Yii app according to the $path.
+    *
+    * @param string $path
+    * @return array
+    * @throws \yii\base\InvalidParamException configuration is empty for this
+    * path
+    */
+    public function getAppConfig()
     {
-        $config = $this->getConfigByPath($path);
+        $config = $this->getConfigByPath();
         if (isset($config[self::APP_KEY]) && !empty($config[self::APP_KEY])) {
             return $config[self::APP_KEY];
         }
-        return [];
+        throw new \yii\base\InvalidParamException("yii app configuration is empty for the path '$path'.");
+    }
+
+    public function getTestCaseDirPath()
+    {
+        return $this->testCaseDirPath;
     }
 
     /**
-     * Set config for all tests @see $allTestsConfig
+     * Setter for the @see $globalConfig property.
      *
-     * Config checks:
+     * This method will verify the $config argument:
      * - it must be an array or a string;
      * - if a string it must be the path to a file returning an array;
      * - if an array it cannot be empty;
@@ -161,134 +203,125 @@ class AppConfigProviderByPath extends \yii\base\Object
      *
      *
      * @param string|array $config
-     * @see $allTestsConfig
+     * @see $globalConfig
      */
-    protected function setAllTestsConfig($config)
+    protected function setGlobalConfig($config)
     {
-        $config = $this->getConfigAsArray($config);
+        $config = $this->configToArray($config);
 
-        if (empty($config)) {
-            throw new InvalidConfigException("Config as an array cannot be empty.");
+        if ([] === $config) {
+            throw new InvalidConfigException("Global config cannot be empty.", 30);
         }
 
-        foreach ($config as $path => &$data) {
+        foreach ($config as $path => $data) {
 
             if (!is_dir($path)) {
                 throw new InvalidConfigException("Each key of the configuration"
-                        . " array must be a path to a tests directory. The path '$path' is invalid.");
+                        . " array must be a path to a tests directory. The path '$path' is invalid.", 40);
             }
 
-            // bootstrap files config check
+            // check bootstrap files config: must be an array of file paths
             if (isset($data[self::BOOTSTRAP_FILES_KEY])) {
 
                 if (is_array($data[self::BOOTSTRAP_FILES_KEY])) {
                     foreach ($data[self::BOOTSTRAP_FILES_KEY] as $bootstrapFile) {
                         if (!is_file($bootstrapFile)) {
-                            throw new InvalidConfigException("Invalid bootstrap file '$bootstrapFile'.");
+                            throw new InvalidConfigException("Invalid bootstrap file '$bootstrapFile'.", 51);
                         }
                     }
                 } else {
-                    throw new InvalidConfigException("Bootstrap files must be listed in an array.");
+                    throw new InvalidConfigException("Bootstrap files must be listed in an array.", 50);
                 }
 
             }
 
-            // server vars config check
+            // check server vars config: must be an array
             if (isset($data[self::SERVER_VARS_KEY])) {
 
-                if (is_array($data[self::SERVER_VARS_KEY])) {
-                    foreach ($data[self::SERVER_VARS_KEY] as $key => $value) {
-                        $_SERVER[$key] = $value;
-                    }
-                } else {
-                    throw new InvalidConfigException("Server vars must be listed in an array.");
+                if (!is_array($data[self::SERVER_VARS_KEY])) {
+                    throw new InvalidConfigException("Server vars must be listed in an array.", 60);
                 }
             }
 
-            // app config check
+            // check Yii app config:
             if (isset($data[self::APP_KEY])) {
-                $data[self::APP_KEY] = $this->getConfigAsArray($data[self::APP_KEY]);
+
+                $mergedAppConfig = [];
+
+                foreach ($data[self::APP_KEY] as $appConfig) {
+                    $appConfigArray = $this->configToArray($appConfig);
+                    $mergedAppConfig = \yii\helpers\ArrayHelper::merge($mergedAppConfig, $appConfigArray);
+                }
+                $config[$path][self::APP_KEY] = $mergedAppConfig;
             }
         }
-
-        $this->allTestsConfig = $config;
+        $this->globalConfig = $config;
     }
-
-    private $lastTestCaseDirPath;
-    private $lastConfigByPath;
+    private $_configByPath;
 
     /**
-     * Extract and merge configs - from @see $allTestsConfig - that match the
-     * $testCaseDirPath argument.
+     * Get the config - bootstrap files, server variables and Yii app config -
+     * that matches the $path argument.
      *
-     * @param string $testCaseDirPath path to the parent directory of the
-     * currently executed test case
+     * This method will search for all paths in the @see $globalConfig that are
+     * parents of the $path argument and merge their configs.
+     *
+     * @see searchGlobalConfigMatchingPaths
+     *
+     * @param string $path the path to the directory of the tested case
+     *
      * @return array
      */
-    protected function getConfigByPath($testCaseDirPath)
+    protected function getConfigByPath()
     {
-        if ($testCaseDirPath !== $this->lastTestCaseDirPath) {
-
-            if (!is_dir($testCaseDirPath)) {
-                throw new \yii\base\InvalidParamException("Path to test case directory '$testCaseDirPath' is invalid.");
-            }
-
+        if (null === $this->_configByPath) {
+            $globalConfigMatchingPaths = $this->searchGlobalConfigMatchingPaths();
+            sort($globalConfigMatchingPaths);
             $configByPath = [];
-            $matchingConfigPaths = $this->getMatchingConfigPaths($testCaseDirPath);
-            sort($matchingConfigPaths);
-
-
-            foreach ($matchingConfigPaths as $matchingConfigPath) {
-                $configByPath = \yii\helpers\ArrayHelper::merge($configByPath, $this->allTestsConfig[$matchingConfigPath]);
+            foreach ($globalConfigMatchingPaths as $globalConfigPath) {
+                $configByPath = \yii\helpers\ArrayHelper::merge($configByPath, $this->globalConfig[$globalConfigPath]);
             }
-
-            $this->lastTestCaseDirPath = $testCaseDirPath;
-            $this->lastConfigByPath = $configByPath;
+            $this->_configByPath = $configByPath;
         }
-
-        return $this->lastConfigByPath;
+        return $this->_configByPath;
     }
 
     /**
-     * Return config paths, i.e. keys of the @see $allTestsConfig, that are
-     * ancestors of the $path argument or equal to it.
+     * Search in the @see $globalConfig root keys for paths that are equal or
+     * ancestors of the @see $testCaseDirPath.
      *
-     * @param string $testCaseDirPath path to the parent directory of the
-     * currently executed test case
+     * If $path is '/var/www/myApp/frontend' the returned array will contain:
+     * - '/var/www/myApp/tests';
+     * - '/var/www/myApp/tests/frontend'
+     *
      * @return array [$path, ...]
      */
-    protected function getMatchingConfigPaths($testCaseDirPath)
+    protected function searchGlobalConfigMatchingPaths()
     {
-        $matchingConfigPaths = [];
-        foreach (array_keys($this->allTestsConfig) as $configPath) {
-            if (false !== strpos($testCaseDirPath, rtrim($configPath, '/'))) {
-                $matchingConfigPaths[] = $configPath;
+        $matchingPaths = [];
+        foreach ($this->globalConfig as $path => $value) {
+            if (false !== strpos($this->testCaseDirPath, rtrim($path, '/'))) {
+                $matchingPaths[] = $path;
             }
         }
-        return $matchingConfigPaths;
+        return $matchingPaths;
     }
 
     /**
-     * Return config as an array.
+     * Return the $config argument as an array.
+     *
+     * Verify that the $config argument is an array or a string.
+     * If the latter it must be the path to an existing file returning an array.
      *
      * @param string|array $config
      * @return array
      * @throws InvalidConfigException config cannot be resolved as an array
      */
-    protected function getConfigAsArray($config)
+    protected function configToArray($config)
     {
-        if (is_string($config)) {
-            if (is_file($config)) {
-                $config = include $config;
-            } else {
-                throw new InvalidConfigException("Config file '$config' does not exist.");
-            }
-        }
-
-        if(is_array($config)){
-            return $config;
-        }
-
-        throw new InvalidConfigException("Config must be an array or the path to a file returning an array.", 40);
+        if (is_string($config) && is_file($config)) $config = include $config;
+        if (is_array($config)) return $config;
+        throw new InvalidConfigException("Config must be an array or the path to a file returning an array.", 20);
     }
+
 }
