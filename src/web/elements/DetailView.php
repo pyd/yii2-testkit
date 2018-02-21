25f9f40caadd62e27343d801ec35b9f7cb88960d
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-02-21 10:42:18 +0100

    Improve NoSuchElementException message in DetailView::getValueByLabel()

--- src/web/elements/DetailView.php
+++ src/web/elements/DetailView.php
@@ -2,9 +2,18 @@
 namespace pyd\testkit\web\elements;
 
 use pyd\testkit\AssertionMessage;
+use NoSuchElementException;
 
 /**
  * Yii DetailView web element.
+ * 
+ * Detail view format is:
+ * <table>
+ *  <tbody>
+ *   <tr>
+ *    <th>label</th>
+ *    <td>value</td>
+ *   </tr>
  *
  * @author Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
  */
@@ -13,21 +22,17 @@ class DetailView extends \pyd\testkit\web\Element
     /**
      * Get the value of a row identified by its label.
      * 
-     * @param string $label
-     * @return string
+     * @param string $label row label
+     * @return \pyd\testkit\web\base\Element
+     * @throws NoSuchElementException
      */
     public function getValueByLabel($label)
     {
-        /*
-         * detail view format is:
-         * <table>
-         *  <tbody>
-         *   <tr>
-         *    <th>label</th>
-         *    <td>value</td>
-         *   </tr>
-         */
-        return $this->findElement(\WebDriverBy::xpath('//th[contains(text(), "' . $label . '")]/following-sibling::td'))->getText();
+        try {
+            return $this->findElement(\WebDriverBy::xpath('//th[contains(text(), "' . $label . '")]/following-sibling::td'))->getText();
+        } catch (NoSuchElementException $e) {
+            throw new NoSuchElementException("Detail view does not have a '$label' label.");
+        }
     }
     
     /**
