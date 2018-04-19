0be8c9550a2a8856f2291bd61e0e1890704e3d7a
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-04-19 10:50:44 +0200

    Add an interface for class managing Yii app as a fixture

---
+++ src/interfaces/InterfaceYiiAppManager.php
@@ -0,0 +1,14 @@
+<?php
+namespace pyd\testkit\interfaces;
+
+/**
+ * Interface for class that manages Yii app as a fixture.
+ *
+ * @author Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
+ */
+interface InterfaceYiiAppManager
+{
+    public function createYiiApp();
+    
+    public function destroyYiiApp();
+}
