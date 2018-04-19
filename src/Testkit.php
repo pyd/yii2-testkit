9948b84c35220509f3371dac26f6cbbc5244ff47
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-04-19 10:27:31 +0200

    Add a Testkit class to store the tstkit Application instance: Testkit::

---
+++ src/Testkit.php
@@ -0,0 +1,15 @@
+<?php
+namespace pyd\testkit;
+
+/**
+ * Description of Testkit
+ *
+ * @author Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
+ */
+class Testkit
+{
+    /**
+     * @var \pyd\testkit\Application
+     */
+    public static $app;
+}
