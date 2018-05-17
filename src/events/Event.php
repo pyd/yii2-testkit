ada1dd836867441ebbdb584cd1fdd0f54ba79841
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-05-17 10:41:32 +0200

    Add an abstract as a base for event classes

---
+++ src/events/Event.php
@@ -0,0 +1,31 @@
+<?php
+namespace pyd\testkit\events;
+
+/**
+ * Base class for events.
+ *
+ * @author Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
+ */
+abstract class Event
+{
+    /**
+     * @var mixed data related to this event 
+     */
+    protected $data;
+    
+    /**
+     * @param mixed $data {@see $data}
+     */
+    public function __construct($data = null)
+    {
+        $this->data = $data;
+        $this->init();
+    }
+    
+    protected function init();
+    
+    /**
+     * @return string name of this event
+     */
+    abstract public static function name();
+}
