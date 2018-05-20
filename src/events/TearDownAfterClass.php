609b96347b59ac111d054d7b5bc7ab7e0246b565
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-05-20 11:02:35 +0200

    Rewrite 'TearDownAfterClass' class after its parent class 'Event' was modified

--- src/events/TearDownAfterClass.php
+++ src/events/TearDownAfterClass.php
@@ -1,18 +1,49 @@
 <?php
 namespace pyd\testkit\events;
 
+use pyd\testkit\events\Event;
+
 /**
  * Event triggered by {@see pyd\testkit\TestCase::tearDownAfterClass()}.
  *
  * @author Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
  */
-class TearDownAfterClass extends SetUpBeforeClass
+class TearDownAfterClass extends Event
 {
+    /**
+     * @var class name of the currently processed test case 
+     */
+    protected $testCaseClass;
+    
+    /**
+     * @todo php7 set type 'string' for $testCaseClass params
+     * 
+     * @param string $testCaseClass class name of the test case for which this
+     * event is triggered
+     * @throws \yii\base\InvalidParamException if $testCaseClass is not an
+     * existing class or is not a class that extends {@see pyd\testkit\TestCase}
+     */
+    public function __construct($testCaseClass)
+    {
+        // if false an exception will be thrown
+        if (Helper::isTestCaseClassName($testCaseClass, true)) {
+            $this->testCaseClass = $testCaseClass;
+        }
+    }
+    
     /**
      * @return string this event name
      */
     public static function name()
     {
         return 'tearDownAfterClass';
+    }
+    
+    /**
+     * @return string the class name of the test case that triggered this event
+     */
+    public function getTestCaseClass()
+    {
+        return $this->testCaseClass;
     }
 }
