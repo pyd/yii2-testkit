02409b48205e1d22357c8c433980b734aa781766
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-05-22 12:07:11 +0200

    Add a class for the 'testCaseEnd' event

---
+++ src/events/TestCaseEnd.php
@@ -0,0 +1,26 @@
+<?php
+namespace pyd\testkit\events;
+
+/**
+ * Event triggered when a test case ends by
+ * {@see \pyd\testkit\Testlistener::endTestSuite()}.
+ *
+ * @author Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
+ */
+class TestCaseEnd extends Event
+{
+    /**
+     * @var \PHPUnit_Framework_TestSuite
+     */
+    protected $suite;
+    
+    public function __construct(\PHPUnit_Framework_TestSuite $suite)
+    {
+        $this->suite = $suite;
+    }
+    
+    public static function name()
+    {
+        return 'testCaseEnd';
+    }
+}
