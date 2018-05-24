3690bf710bf7dff056a205bb3a1095145d532415
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-05-24 09:58:00 +0200

    Cleanup TestListener

--- src/TestListener.php
+++ src/TestListener.php
@@ -11,68 +11,35 @@ use pyd\testkit\Testkit;
  */
 class TestListener extends \PHPUnit_Framework_BaseTestListener
 {
-    const EVENT_TEST_SUITE_START = 'testSuiteStart';
-    const EVENT_TEST_CASE_START = 'testCaseStart';
-    const EVENT_TEST_CASE_END = 'testCaseEnd';
-    const EVENT_TEST_SUITE_END = 'testSuiteEnd';
-    const EVENT_TEST_METHOD_START = 'testMethodStart';
-    const EVENT_TEST_METHOD_END = 'testMethodEnd';
-    
     /**
      * @see startTestSuite
      * @see \PHPUnit_Framework_TestSuite::getName()
      * @var string test suite name i.e. a test case class name or a path/to/dir
      * depending on the target passed to the phpunit command
      */
-    protected $testSuiteName;
+    protected $suiteName;
     
     /**
-     * Listen to the {@see EVENT_TEST_SUITE_START} and {@see EVENT_TEST_CASE_START}
-     * events and inform the test events dispatcher with the
-     * {@see \PHPUnit_Framework_TestSuite::$name} property as data.
+     * Start of a test suite or of a test case.
      * 
      * @param \PHPUnit_Framework_TestSuite $suite
+     * 
      */
     public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
     {
         $suiteName = $suite->getName();
         
-        if (null === $this->testSuiteName) {
-            $this->informTestEventMediator(self::EVENT_TEST_SUITE_START, $suiteName);
-            $this->testSuiteName = $suiteName;
+        if (null === $this->suiteName) {
+            $this->suiteName = $suiteName;
         }
+        
         if (class_exists($suiteName)) {
-            $this->informTestEventMediator(self::EVENT_TEST_CASE_START, $suiteName);
+            // inform observers of 'startTestCase' event
         }
     }
-    
-    /**
-     * Listen to the {@see EVENT_TEST_METHOD_START} event and inform the test
-     * events dispatcher with the {@see \pyd\testkit\TestCase} instance as data.
-     * 
-     * @param \PHPUnit_Framework_Test $test
-     */
-    public function startTest(\PHPUnit_Framework_Test $test)
-    {
-        $this->informTestEventMediator(self::EVENT_TEST_METHOD_START, $test);
-    }
 
     /**
-     * Listen to the {@see EVENT_TEST_METHOD_START} event and inform the test
-     * events dispatcher with the {@see \pyd\testkit\TestCase} instance as data.
-     * 
-     * @param \PHPUnit_Framework_Test $test
-     * @param float $time
-     */
-    public function endTest(\PHPUnit_Framework_Test $test, $time)
-    {
-        $this->informTestEventMediator(self::EVENT_TEST_METHOD_END, $test);
-    }
-
-    /**
-     * Listen to the {@see EVENT_TEST_SUITE_END} and {@see EVENT_TEST_CASE_END}
-     * events and inform the test events dispatcher with the
-     * {@see \PHPUnit_Framework_TestSuite::$name} property as data.
+     * End of a test suite or of a test case.
      * 
      * @param \PHPUnit_Framework_TestSuite $suite
      */
@@ -81,21 +48,21 @@ class TestListener extends \PHPUnit_Framework_BaseTestListener
         $suiteName = $suite->getName();
         
         if (class_exists($suiteName)) {
-            $this->informTestEventMediator(self::EVENT_TEST_CASE_END, $suiteName);
+            $this->informObservers(new events\EndTestCase($suite));
         }
-        if ($suiteName = $this->testSuiteName) {
-            $this->informTestEventMediator(self::EVENT_TEST_SUITE_END, $suiteName);
+        
+        if ($suiteName = $this->suiteName) {
+            // inform observers of 'endTestSuite' event.
         }
     }
     
     /**
-     * Inform the test event mediator of a test event.
+     * Inform observers of an event.
      * 
-     * @param string $eventName 
-     * @param mixed $data
+     * @param \pyd\testkit\events\Event $event 
      */
-    protected function informTestEventMediator($eventName, $data = null)
+    protected function informObservers(events\Event $event)
     {
-        Testkit::$app->testEventMediator->trigger($eventName, $data);
+        Testkit::$app->eventMediator->informObservers($event);
     }
 }
