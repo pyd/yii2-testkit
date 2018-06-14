bb59295a8c723055224406a7ffd1e6213142c53b
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-06-14 18:34:25 +0200

    Cleanup Breadcrumbs element class and add a getLabels() method

--- src/web/elements/Breadcrumbs.php
+++ src/web/elements/Breadcrumbs.php
@@ -11,8 +11,12 @@ class Breadcrumbs extends \pyd\testkit\web\Element
     /**
      * @var array \pyd\testkit\functional\Element
      */
-    protected $items;
+    private $_items;
 
+    /**
+     * Add locator:
+     * - array: items in the breadcrumbs element;
+     */
     protected function initLocators()
     {
         parent::initLocators();
@@ -20,29 +24,62 @@ class Breadcrumbs extends \pyd\testkit\web\Element
     }
 
     /**
+     * Find all items.
+     * 
      * @return array \pyd\testkit\functional\Element
      */
-    public function getItems()
+    public function findItems()
     {
-        if (null === $this->items) {
-            $this->items = $this->findElements('items');
+        if (null === $this->_items) {
+            $this->_items = $this->findElements('items');
         }
-        return $this->items;
+        return $this->_items;
     }
 
-    public function hasLabel($label)
+    /**
+     * One item has the expected label.
+     * 
+     * @param string $label
+     * @param boolean $caseSensitive 
+     * @return boolean
+     */
+    public function hasLabel($label, $caseSensitive = false)
     {
-        foreach ($this->getItems() as $item) {
-            if (0 === strcasecmp($label, $item->getText())) return true;
+        $method = $caseSensitive ? 'strcmp' : 'strcasecmp';
+        foreach ($this->findItems() as $item) {
+            return (0 === $method($label, $item->getText()));
         }
         return false;
     }
 
-    public function labelContainsText($texte)
+    /**
+     * One item has a label that contains - may be all label or just a part of
+     * it - the expected text.
+     * 
+     * @param string $texte
+     * @return boolean
+     */
+    public function labelContainsText($texte, $caseSensitive = false)
     {
-        foreach ($this->getItems() as $item) {
-            if (false !== strpos($item->getText(), $texte)) return true;
+        $method = $caseSensitive ? 'strpos' : 'stripos';
+        foreach ($this->findItems() as $item) {
+            return (false !== $method($item->getText(), $texte));
         }
         return false;
     }
+    
+    /**
+     * Get all labels as an array of strings.
+     * 
+     * @return array
+     */
+    public function getLabels()
+    {
+        $labels = [];
+        foreach ($this->findItems() as $item) {
+            $labels[] = $item->getText();
+        }
+        return $labels;
+    }
+
 }
