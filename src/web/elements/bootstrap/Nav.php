74f43de39c451dc103bd65851f4ea8ff428b7c83
Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
2018-06-24 14:24:26 +0200

    Create class for bootstrap 3 'nav' element

---
+++ src/web/elements/bootstrap/Nav.php
@@ -0,0 +1,32 @@
+<?php
+namespace pyd\testkit\web\elements\bootstrap;
+
+/**
+ * Bootstrap 3 nav element.
+ * 
+ * @property array $items menu items
+ *
+ * @author Pierre-Yves DELETTRE <pierre.yves.delettre@gmail.com>
+ */
+class Nav extends \pyd\testkit\web\Element
+{
+    protected function initLocators()
+    {
+        parent::initLocators();
+        $this->locator->add('items', \WebDriverBy::tagName('li'));
+    }
+    
+    /**
+     * Get all items labels.
+     * 
+     * @return array
+     */
+    public function getLabels()
+    {
+        $labels = [];
+        foreach ($this->findElements(\WebDriverBy::tagName('li')) as $item) {
+            $labels[] = $item->getText();
+        }
+        return $labels;
+    }
+}
